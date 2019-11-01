0bc3c4aa94171807f0873b229b2645fc42bb0b08
jion-kozono <zeon66190329@gmail.com>
2019-11-01 13:05:14 +0900

    チャプター１０間で終了 2019_11_01

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
