aace5bb2ec680f08de661bad2d6b779f336565b8
jion-kozono <zeon66190329@gmail.com>
2019-11-05 14:32:19 +0900

    emil-config 2019_11_05

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -132,10 +132,3 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
